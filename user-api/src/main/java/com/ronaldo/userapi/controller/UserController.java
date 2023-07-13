6d448b86c5933ee121042da82d4475af6afe985a
Ronaldo Gregorio <gr3goriowr@gmail.com>
2023-07-13 14:48:40 -0300

    Removendo usuários

--- user-api/src/main/java/com/ronaldo/userapi/controller/UserController.java
+++ user-api/src/main/java/com/ronaldo/userapi/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.*;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,11 @@ public class UserController {
 	@GetMapping("/{cpf}")
 	public UserDTO getUsersFiltro(@PathVariable String cpf) {
 				return usuarios.stream().filter(UserDTO -> UserDTO.getCpf().equals(cpf)).findFirst().orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+	}
+	
+	@DeleteMapping("/{cpf}")
+	public boolean remove(@PathVariable String cpf) {
+				return usuarios.removeIf(userDTO -> userDTO.getCpf().equals(cpf));
 	}
 	
 	@PostMapping
--- user-api/src/main/java/com/ronaldo/userapi/response/UserDTO.java
+++ user-api/src/main/java/com/ronaldo/userapi/response/UserDTO.java
@@ -1,14 +1,11 @@
 package com.ronaldo.userapi.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
