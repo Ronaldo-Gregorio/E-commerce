bfa39374ec33a7acb0defd14eacf45bc22f4fc6f
Ronaldo Gregorio <gr3goriowr@gmail.com>
2023-07-13 14:04:54 -0300

    Iserindo usuários

--- user-api/src/main/java/com/ronaldo/userapi/controller/UserController.java
+++ user-api/src/main/java/com/ronaldo/userapi/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.ronaldo.userapi.controller;
-import com.ronaldo.userapi.*;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ronaldo.userapi.response.*;
+
 import jakarta.annotation.PostConstruct;
 
 @RestController
@@ -50,5 +56,18 @@ public class UserController {
 	@GetMapping
 	public List<UserDTO> getUsers(){
 		return usuarios;
+	}
+	
+	@GetMapping("/{cpf}")
+	public UserDTO getUsersFiltro(@PathVariable String cpf) {
+				return usuarios.stream().filter(UserDTO -> UserDTO.getCpf().equals(cpf)).findFirst().orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public UserDTO inserir(@RequestBody UserDTO userDTO) {
+		userDTO.setDataCadastro(LocalDateTime.now());
+		usuarios.add(userDTO);
+		return userDTO;
 	}
 }
