ed94a9104df0fcbb8f3c43ef45bfbdcee49a5f87
Ronaldo Gregorio <gr3goriowr@gmail.com>
2023-07-13 02:56:47 -0300

    Iniciando o projeto de e-commerce

---
+++ user-api/src/test/java/com/ronaldo/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldo.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
