7438ba4bc6a4b6fd14de277d8fcb35f92c331c45
Ronaldo Gregorio <gr3goriowr@gmail.com>
2023-07-14 13:28:46 -0300

    Camada de dados(Repository)

--- user-api/src/main/java/com/ronaldo/userapi/response/UserDTO.java
+++ user-api/src/main/java/com/ronaldo/userapi/response/UserDTO.java
@@ -1,17 +1,15 @@
 package com.ronaldo.userapi.response;
 
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
